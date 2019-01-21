3bd7216f5d91b67107743a907785ec958ede8f2f
tutkuince <tutku.ince@outlook.com>
2019-01-21 21:03:31 +0300

    specify plural name/path with an @RepositoryRestResource(path='students')

--- src/main/java/com/muditasoft/springbootrest/dao/StudentRepository.java
+++ src/main/java/com/muditasoft/springbootrest/dao/StudentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.muditasoft.springbootrest.model.Student;
 
+// @RepositoryRestResource(path = "students")   -> Specify plural name / path with an annotation
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
