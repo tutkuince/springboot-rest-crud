9c89bbd12469428b582e66cdc47eed60325bbc8c
tutkuince <tutku.ince@outlook.com>
2019-01-21 15:27:18 +0300

    initial commit

---
+++ src/main/java/com/muditasoft/springbootrest/SpringbootRestCrudApplication.java
@@ -0,0 +1,14 @@
+package com.muditasoft.springbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestCrudApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/muditasoft/springbootrest/SpringbootRestCrudApplicationTests.java
@@ -0,0 +1,17 @@
+package com.muditasoft.springbootrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
