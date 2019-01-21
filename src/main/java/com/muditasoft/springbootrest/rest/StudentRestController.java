d236cac463942a98cd2eeaa804ebceeffa3cae0b
tutkuince <tutku.ince@outlook.com>
2019-01-21 18:40:27 +0300

    PutMapping for update existing student in updateStudent method

--- src/main/java/com/muditasoft/springbootrest/rest/StudentRestController.java
+++ src/main/java/com/muditasoft/springbootrest/rest/StudentRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,8 +45,16 @@ public class StudentRestController {
 		return student;
 	}
 	
+	// add mapping for POST /students - add new student
 	@PostMapping("/students")
 	public Student addStudent(@RequestBody Student student) {
+		studentService.saveOrUpdate(student);
+		return student;
+	}
+	
+	// add mapping for PUT /students - update existing student
+	@PutMapping("/students")
+	public Student updateStudent(@RequestBody Student student) {
 		studentService.saveOrUpdate(student);
 		return student;
 	}
