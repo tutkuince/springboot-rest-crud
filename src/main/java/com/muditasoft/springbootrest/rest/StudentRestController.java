51e6e7093426b720679850d84a77938545d88df4
tutkuince <tutku.ince@outlook.com>
2019-01-21 17:54:30 +0300

    getStudentById

--- src/main/java/com/muditasoft/springbootrest/rest/StudentRestController.java
+++ src/main/java/com/muditasoft/springbootrest/rest/StudentRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,10 @@ public class StudentRestController {
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		return studentService.findAll();
+	}
+	
+	@GetMapping("/students/{id}")
+	public Student getStudentById(@PathVariable(name = "id") Long id) {
+		return studentService.findById(id);
 	}
 }
