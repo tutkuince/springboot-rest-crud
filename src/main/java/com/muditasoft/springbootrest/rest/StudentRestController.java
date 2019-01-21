16c37cf2ce4c11ed2bf21907f9f9f19ad315e316
tutkuince <tutku.ince@outlook.com>
2019-01-21 18:14:55 +0300

    addStudent with RequestBody

--- src/main/java/com/muditasoft/springbootrest/rest/StudentRestController.java
+++ src/main/java/com/muditasoft/springbootrest/rest/StudentRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +41,16 @@ public class StudentRestController {
 		if(student == null)
 			throw new RuntimeException("Student id not found - " + id);
 		
+		return student;
+	}
+	
+	@PostMapping("/students/{id}")
+	public Student addStudent(@RequestBody Student student) {
+		// also just in case they pass an id in JSON . . . set id to 0
+		// this is to force a save of new item . . . instead of update
+		
+		student.setId(0L);
+		studentService.saveOrUpdate(student);
 		return student;
 	}
 }
